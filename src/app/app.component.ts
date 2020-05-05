37954066a8fa1cc542c73dccbdf26db2b068f518
maknetaRo <makneta@gmail.com>
2020-05-05 17:59:21 +0200

    start angular app, create digital watch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'timer';
+  time: number;
+
+  constructor() {
+    console.log(this.time);
+    setInterval(() => {
+      this.time = Date.now();
+    }, 1);
+  }
 }
